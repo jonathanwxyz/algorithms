ef29b28a9f219c161b52e1e3ee5a29776f502e34
Jonathan <mail4jonathanw@gmail.com>
2022-06-24 01:07:22 +0100

    optimised concurrent merge sort and fixed benchmark

--- mergesort/mergesort.go
+++ mergesort/mergesort.go
@@ -1,54 +1,97 @@
 package main
 
 import (
-	"fmt"
 	"sync"
+    "runtime"
 )
 
 type Comparable interface {
 	string | int | int8 | int16 | int32 | int64 | float32 | float64 | uint | uint8 | uint16 | uint32 | uintptr
 }
 
-func ConMergeSort[C Comparable](list []C) []C {
-	if len(list) > 1 {
-		var half int
-		if len(list)%2 == 1 {
-			half = (len(list) + 1) / 2
-		} else {
-			half = len(list) / 2
-		}
-		var l1, l2 []C
-		var waitGroup sync.WaitGroup
-		waitGroup.Add(2)
-		go func() {
-			defer waitGroup.Done()
-			l1 = ConMergeSort(list[0:half])
-		}()
-		go func() {
-			defer waitGroup.Done()
-			l2 = ConMergeSort(list[half:])
-		}()
-		waitGroup.Wait()
-		return merge(l1, l2)
-	}
-	return list
+func MergeSort[C Comparable](arr []C) []C {
+    if len(arr) < 512 {
+        return SeqMergeSort(arr)
+    }
+    numCores := runtime.NumCPU()
+    return ConLowMergeSort(arr, numCores)
 }
 
-func MergeSort[C Comparable](list []C) []C {
-	if len(list) > 1 {
+// ConLowMergeSort: Concurrent Low-goroutine Merge Sort
+func ConLowMergeSort[C Comparable](arr []C, coroutines int) []C {
+	if len(arr) <= 1 {
+        return arr
+    }
+
+    half := len(arr) / 2
+
+    if coroutines <= 0 {
+        return merge(SeqMergeSort(arr[0:half]), SeqMergeSort(arr[half:]))
+    }
+
+    var l1, l2 []C
+    var waitGroup sync.WaitGroup
+    waitGroup.Add(2)
+
+    go func() {
+        defer waitGroup.Done()
+        l1 = ConLowMergeSort(arr[0:half], coroutines - 2)
+    }()
+    go func() {
+        defer waitGroup.Done()
+        l2 = ConLowMergeSort(arr[half:], coroutines - 2)
+    }()
+
+    waitGroup.Wait()
+    return merge(l1, l2)
+}
+
+// SeqMergeSort: Sequential Merge Sort
+func SeqMergeSort[C Comparable](arr []C) []C {
+	if len(arr) > 1 {
 		var half int
-		if len(list)%2 == 1 {
-			half = (len(list) + 1) / 2
+		if len(arr)%2 == 1 {
+			half = (len(arr) + 1) / 2
 		} else {
-			half = len(list) / 2
+			half = len(arr) / 2
 		}
-		return merge(MergeSort(list[0:half]), MergeSort(list[half:]))
+		return merge(SeqMergeSort(arr[0:half]), SeqMergeSort(arr[half:]))
 	}
-	return list
+	return arr
+}
+
+// ConMergeSort: Concurrent Merge Sort
+func ConMergeSort[C Comparable](arr []C) []C {
+
+	if len(arr) <= 1 {
+        return arr
+    }
+
+    half := len(arr) / 2
+    // performance optimisation
+    if len(arr) <= 1024 {
+        return merge(SeqMergeSort(arr[0:half]), SeqMergeSort(arr[half:]))
+    }
+
+    var l1, l2 []C
+    var waitGroup sync.WaitGroup
+    waitGroup.Add(2)
+
+    go func() {
+        defer waitGroup.Done()
+        l1 = ConMergeSort(arr[0:half])
+    }()
+    go func() {
+        defer waitGroup.Done()
+        l2 = ConMergeSort(arr[half:])
+    }()
+
+    waitGroup.Wait()
+    return merge(l1, l2)
 }
 
 func merge[C Comparable](l1, l2 []C) []C {
-	sorted := []C{}
+    sorted := make([]C, 0, len(l1) + len(l2))
 	i, j := 0, 0
 	for i != len(l1) || j != len(l2) {
 		if i == len(l1) {
--- mergesort/mergesort_test.go
+++ mergesort/mergesort_test.go
@@ -25,29 +25,6 @@ func TestMerge(t *testing.T) {
 	}
 }
 
-func TestConMergeSort(t *testing.T) {
-	tables := []struct {
-		q []int
-		a []int
-	}{
-		{[]int{}, []int{}},
-		{[]int{3}, []int{3}},
-		{[]int{1, 2, 3}, []int{1, 2, 3}},
-		{[]int{2, 1, 3}, []int{1, 2, 3}},
-		{[]int{3, 1, 2}, []int{1, 2, 3}},
-		{[]int{5, 7, 10, 22, 1, 6}, []int{1, 5, 6, 7, 10, 22}},
-		{[]int{1, 5, 7, 10, 22, 1, 6}, []int{1, 1, 5, 6, 7, 10, 22}},
-		{[]int{3, 5, 7, 10, 22, 1, 6}, []int{1, 3, 5, 6, 7, 10, 22}},
-	}
-
-	for _, table := range tables {
-		ans := ConMergeSort(table.q)
-		if !intSlicesEqual(ans, table.a) {
-			t.Errorf("Sort of slice %v incorrect, returned %v, expected %v", table.q, ans, table.a)
-		}
-	}
-}
-
 func TestMergeSort(t *testing.T) {
 	tables := []struct {
 		q []int
@@ -71,13 +48,38 @@ func TestMergeSort(t *testing.T) {
 	}
 }
 
+func TestSeqMergeSort(t *testing.T) {
+	tables := []struct {
+		q []int
+		a []int
+	}{
+		{[]int{}, []int{}},
+		{[]int{3}, []int{3}},
+		{[]int{1, 2, 3}, []int{1, 2, 3}},
+		{[]int{2, 1, 3}, []int{1, 2, 3}},
+		{[]int{3, 1, 2}, []int{1, 2, 3}},
+		{[]int{5, 7, 10, 22, 1, 6}, []int{1, 5, 6, 7, 10, 22}},
+		{[]int{1, 5, 7, 10, 22, 1, 6}, []int{1, 1, 5, 6, 7, 10, 22}},
+		{[]int{3, 5, 7, 10, 22, 1, 6}, []int{1, 3, 5, 6, 7, 10, 22}},
+	}
+
+	for _, table := range tables {
+		ans := SeqMergeSort(table.q)
+		if !intSlicesEqual(ans, table.a) {
+			t.Errorf("Sort of slice %v incorrect, returned %v, expected %v", table.q, ans, table.a)
+		}
+	}
+}
+
 func benchmarkMergeSort(s int, b *testing.B) {
 	for j := 0; j < b.N; j++ {
-		testSlice := []int{}
+        testSlice := make([]int, 0, s)
 		for i := 0; i < s; i++ {
 			testSlice = append(testSlice, rand.Int())
 		}
+        b.StartTimer()
 		MergeSort(testSlice)
+        b.StopTimer()
 	}
 }
 
@@ -87,25 +89,50 @@ func BenchmarkMergeSort1000(b *testing.B)      { benchmarkMergeSort(1000, b) }
 func BenchmarkMergeSort10000(b *testing.B)     { benchmarkMergeSort(10000, b) }
 func BenchmarkMergeSort100000(b *testing.B)    { benchmarkMergeSort(100000, b) }
 func BenchmarkMergeSort1000000(b *testing.B)   { benchmarkMergeSort(1000000, b) }
-func BenchmarkMergeSort100000000(b *testing.B) { benchmarkMergeSort(100000000, b) }
+func BenchmarkMergeSort10000000(b *testing.B)  { benchmarkMergeSort(10000000, b) }
+//func BenchmarkMergeSort100000000(b *testing.B) { benchmarkMergeSort(100000000, b) }
 
 func benchmarkConMergeSort(s int, b *testing.B) {
 	for j := 0; j < b.N; j++ {
-		testSlice := []int{}
+        testSlice := make([]int, 0, s)
 		for i := 0; i < s; i++ {
 			testSlice = append(testSlice, rand.Int())
 		}
+        b.StartTimer()
 		ConMergeSort(testSlice)
+        b.StopTimer()
 	}
 }
 
-func BenchmarkConMergeSort10(b *testing.B)        { benchmarkMergeSort(10, b) }
-func BenchmarkConMergeSort100(b *testing.B)       { benchmarkMergeSort(100, b) }
-func BenchmarkConMergeSort1000(b *testing.B)      { benchmarkMergeSort(1000, b) }
-func BenchmarkConMergeSort10000(b *testing.B)     { benchmarkMergeSort(10000, b) }
-func BenchmarkConMergeSort100000(b *testing.B)    { benchmarkMergeSort(100000, b) }
-func BenchmarkConMergeSort1000000(b *testing.B)   { benchmarkMergeSort(1000000, b) }
-func BenchmarkConMergeSort100000000(b *testing.B) { benchmarkMergeSort(100000000, b) }
+func BenchmarkConMergeSort10(b *testing.B)        { benchmarkConMergeSort(10, b) }
+func BenchmarkConMergeSort100(b *testing.B)       { benchmarkConMergeSort(100, b) }
+func BenchmarkConMergeSort1000(b *testing.B)      { benchmarkConMergeSort(1000, b) }
+func BenchmarkConMergeSort10000(b *testing.B)     { benchmarkConMergeSort(10000, b) }
+func BenchmarkConMergeSort100000(b *testing.B)    { benchmarkConMergeSort(100000, b) }
+func BenchmarkConMergeSort1000000(b *testing.B)   { benchmarkConMergeSort(1000000, b) }
+func BenchmarkConMergeSort10000000(b *testing.B)  { benchmarkConMergeSort(10000000, b) }
+//func BenchmarkConMergeSort100000000(b *testing.B) { benchmarkConMergeSort(100000000, b) }
+
+func benchmarkSeqMergeSort(s int, b *testing.B) {
+	for j := 0; j < b.N; j++ {
+        testSlice := make([]int, 0, s)
+		for i := 0; i < s; i++ {
+			testSlice = append(testSlice, rand.Int())
+		}
+        b.StartTimer()
+		SeqMergeSort(testSlice)
+        b.StopTimer()
+	}
+}
+
+func BenchmarkSeqMergeSort10(b *testing.B)        { benchmarkSeqMergeSort(10, b) }
+func BenchmarkSeqMergeSort100(b *testing.B)       { benchmarkSeqMergeSort(100, b) }
+func BenchmarkSeqMergeSort1000(b *testing.B)      { benchmarkSeqMergeSort(1000, b) }
+func BenchmarkSeqMergeSort10000(b *testing.B)     { benchmarkSeqMergeSort(10000, b) }
+func BenchmarkSeqMergeSort100000(b *testing.B)    { benchmarkSeqMergeSort(100000, b) }
+func BenchmarkSeqMergeSort1000000(b *testing.B)   { benchmarkSeqMergeSort(1000000, b) }
+func BenchmarkSeqMergeSort10000000(b *testing.B)  { benchmarkSeqMergeSort(10000000, b) }
+//func BenchmarkSeqMergeSort100000000(b *testing.B) { benchmarkSeqMergeSort(100000000, b) }
 
 func intSlicesEqual(a, b []int) bool {
 	if len(a) != len(b) {
